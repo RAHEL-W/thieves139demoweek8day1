be346f20e9ce9902650132ac420868041bf46256
RAHEL <richassefa27@gmail.com>
2024-02-26 19:49:48 -0800

    homework

--- dist/homework.js
+++ dist/homework.js
@@ -8,3 +8,22 @@ let d = {
 let e = [3];
 let f;
 let g = [];
+const Square = (n) => {
+    let sum = 0;
+    for (let i = 0; i < n.length; i++) {
+        sum += n[i] ** 2;
+    }
+    return sum;
+};
+console.log(Square([1, 2, 3, 5]));
+const boolean_to_word = (boole) => {
+    let result = '';
+    if (boole === true) {
+        result = 'yes';
+    }
+    else if (boole === false) {
+        result = 'No';
+    }
+    return result;
+};
+console.log(boolean_to_word(true));
